3b641b294cd61158fe973e8ffc6e8931cac7e84b
Rohan1203s <92736010+Rohan1203s@users.noreply.github.com>
2023-05-11 11:54:50 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ button.addEventListener("click", function() {
   }, 1000000); // 5 second delay before hiding the preloader
 });
 
+
 // Add a load event listener to the window
 window.addEventListener("load", function() {
   window.scrollBy(0, 100);
+
 
   const preloader = document.querySelector(".preloader");
   preloader.addEventListener("animationend", function() {
@@ -20,17 +22,24 @@ window.addEventListener("load", function() {
 
 
 
+
+
+
 const canvas = document.querySelector(".canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+
 const context = canvas.getContext("2d");
 const frameCount = 90;
 
+
 const currentFrame = (index) => `./theligght/${(index + 1).toString()}.jpg`;
+
 
 const images = [];
 let ball = { frame: 0 };
+
 
 for (let i = 0; i < frameCount; i++) {
   const img = new Image();
@@ -38,6 +47,7 @@ for (let i = 0; i < frameCount; i++) {
   console.log(currentFrame(i));
   images.push(img);
 }
+
 
 gsap.to(ball, {
   frame: frameCount - 1,
@@ -51,6 +61,7 @@ gsap.to(ball, {
   onUpdate: render,
 });
 
+
 gsap.fromTo(
   ".ball-text",
   {
@@ -60,8 +71,8 @@ gsap.fromTo(
     opacity: 1,
     scrollTrigger: {
       scrub: 1,
-      start: "60%",
-      end: "64%",
+      start: "25%",
+      end: "45%",
     },
     onComplete: () => {
       gsap.to(".ball-text", { opacity: 0 });
@@ -71,7 +82,14 @@ gsap.fromTo(
 );
 
 
+
+
 window.addEventListener('scroll', handleScroll);
+
+
+
+
+
 
 
 
@@ -79,11 +97,12 @@ window.addEventListener('scroll', handleScroll);
 
 images[0].onload = render;
 
+
 function render() {
   context.canvas.width = images[0].width;
   context.canvas.height = images[0].height;
 
+
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.drawImage(images[ball.frame], 0, 0);
 }
-
